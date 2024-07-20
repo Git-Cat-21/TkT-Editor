f62cfc2456616cf8856fad0639b8aef4545d034f
Git-Cat-21 <khushmish21@gmail.com>
2024-07-20 22:11:28 +0530

    bg color changed

--- text_editor.py
+++ text_editor.py
@@ -24,8 +24,10 @@ def main():
     root.geometry("700x700")
     frame=Frame(root)
     frame.grid(row=0, column=0, sticky='e')
-    menubar=Menu(root) #creates menu object and stores it in menubar MAIN MENU BAR
-    file=Menu(menubar,tearoff=0) #menu object called file submenu
+    #creates menu object and stores it in menubar MAIN MENU BAR
+    menubar=Menu(root) 
+    #menu object called file submenu
+    file=Menu(menubar,tearoff=0) 
     #tearoff detach the menu from the main window and turn it into a separate window not detached
     file.add_command(label="New",command=new)
     # file.add_command(label='Open',command=hello)
@@ -34,7 +36,8 @@ def main():
     file.add_command(label='Delete',command=delete)
     file.add_command(label='Exit',command=root.quit)
     menubar.add_cascade(label="Options", menu=file)
-    T=Text(root,height=700,width=700,bg='yellow')
+
+    T=Text(root,height=700,width=700,bg='#e2c6f1')
     T.grid(row=1, column=0, sticky='nsew')
     root.grid_rowconfigure(1, weight=1)
     root.grid_columnconfigure(0, weight=1)
@@ -43,5 +46,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-#save save as cancel open
