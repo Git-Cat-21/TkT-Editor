fb78407520a922b0e26d46b088728f22e24c19d8
Git-Cat-21 <khushmish21@gmail.com>
2024-07-24 21:53:12 +0530

    moved functions

--- file_functions.py
+++ file_functions.py
@@ -87,24 +87,4 @@ def clear_all(self):
     T=get_widget('text_widget')
     T.delete("1.0",END)
 
-def Open_Shortcuts(event=None):
-    shortcut = Tk()
-    shortcut.title("Key Shortcuts")
-    shortcut.geometry("320x280")
-    with open("assets/shortcut.txt", "r") as w:
-        content = w.read()
-    Label(shortcut,text=content, justify="left",font=("Helvetica",12,"italic")).pack()
-    Exit_button=Button(shortcut,text="Exit",command = lambda: close_window(shortcut),font=("Helvetica", 10), bg='#f44336', fg='white')
-    Exit_button.pack(padx=10)
-    shortcut.configure()
-    shortcut.mainloop()
-
-def Open_ReadMe(event=None):
-    T=get_widget('text_widget')
-    global file_path
-    file_path="README.md"
-    with open(file_path,"r") as file_ptr:
-        content=file_ptr.read()
-        T.delete("1.0",END)
-        T.insert("1.0",content)
 
