e8fbeab4d7b2294ff9e3719308d70609dda52140
Git-Cat-21 <khushmish21@gmail.com>
2024-07-26 20:47:02 +0530

    widgets added

--- print_function.py
+++ print_function.py
@@ -2,6 +2,7 @@ from tkinter import *
 from widget_registry import get_widget
 from fpdf import FPDF
 from file_functions import font_name,font_size
+from tkinter.filedialog import askopenfilename
 
 def convert_to_pdf():
     pdf=FPDF()
@@ -10,4 +11,24 @@ def convert_to_pdf():
     T=get_widget("text_widget")
     info=T.get("1.0",END).strip()
     pdf.multi_cell(0, 10, txt=info, align='L')
+
+    root_browse=Tk()
+    root_browse.title("Browse")
+    root_browse.geometry("300x300")
+
+    destination_var=StringVar()
+    file_name_var=StringVar()
+
+    get_file_dest=Entry(root_browse,textvariable=destination_var,font=('calibre',10,'normal'))
+    get_file_dest.pack()
+
+    get_file_name=Entry(root_browse,textvariable=file_name_var,font=('calibre',10,'normal'))
+    get_file_name.pack()
+
+    # browse, submit, exit
+
+    root_browse.mainloop()
+    # askopenfilename()
+
+    
     pdf.output("mypdf1.pdf")
